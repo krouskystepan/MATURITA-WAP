f739d2d6468324b02be09227e61aa4f376845777
Stepan Krousky <stepan.krousky@seznam.cz>
2024-01-26 21:30:06 +0100

    server done

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,4 +1,4 @@
-const User = require('../models/users');
+const User = require('../models/users')
 
 exports.createUser = async (req, res) => {
   try {
@@ -6,50 +6,83 @@ exports.createUser = async (req, res) => {
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       age: req.body.age,
-    });
-    const result = await data.save();
+    })
+    const result = await data.save()
     if (result) {
       return res.status(201).send({
         msg: 'User created',
         payload: result,
-      });
+      })
     }
     res.status(500).send({
       msg: 'User was not created',
-    });
+    })
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error)
   }
-};
+}
 
-//  Tohle je dost simple, je potřeba to upravit a dodělat
 exports.getAllUsers = async (req, res) => {
   try {
-    const users = await User.find({});
-    res.send(users)
+    const result = await User.find()
+    if (result && result.length !== 0) {
+      return res.status(200).send({
+        msg: 'Users found!',
+        payload: result,
+      })
+    }
+    res.status(404).send({ msg: 'Users not found' })
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error)
   }
-};
+}
 
 exports.getUserById = async (req, res) => {
   try {
-    const user = await User.find({_id: req.params.id});
-    res.send(user)
+    const result = await User.findById(req.params.id)
+    if (result) {
+      return res.status(200).send({
+        msg: 'User found!',
+        payload: result,
+      })
+    }
+    res.status(404).send({ msg: 'User not found' })
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error)
   }
-};
+}
 
 exports.deleteUser = async (req, res) => {
   try {
-    await User.deleteOne({ _id: req.params.id });
-    res.send("User deleted")
+    const result = await User.findByIdAndDelete(req.params.id)
+    if (result) {
+      return res.status(200).send({
+        msg: 'User deleted',
+      })
+    }
+    res.status(404).send({ msg: 'Something went wrong' })
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error)
   }
-};
+}
 
-exports.updateUser = (req, res) => {
-  res.send('updated user ' + req.params.id);
-};
+exports.updateUser = async (req, res) => {
+  try {
+    const data = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      age: req.body.age,
+    }
+    const result = await User.findByIdAndUpdate(req.params.id, data)
+    if (result) {
+      return res.status(200).send({
+        msg: 'User updated',
+      })
+    }
+    res.status(500).send({
+      msg: 'User was not updated',
+    })
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
