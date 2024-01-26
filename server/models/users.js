4cf68678207303216c6151c4e4825255be1e7162
Stepan Krousky <stepan.krousky@seznam.cz>
2024-01-26 22:12:52 +0100

    ...

--- server/models/users.js
+++ server/models/users.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 /* 
 mongoose.Schema - vytvoří schéma
  - Struktura:
-      name: { type: String, required: true }  
+      firstName: { type: String, required: true }  
       lastName: { type: String, required: true }
       age: { type: Number, required: true }
 */
