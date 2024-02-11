7acdddfa31e851eac55b8211d65e0c392b9462e0
Stepan Krousky <stepan.krousky@seznam.cz>
2024-02-11 13:56:51 +0100

    fix typo

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,41 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
-const usersConroller = require('../controllers/users');
+const usersController = require('../controllers/users');
 
 /**
  * CREATE USER
  * URL: http://localhost:3000/users
  * Method: POST
  */
-router.post('/', usersConroller.createUser);
+router.post('/', usersController.createUser);
 
 /**
  * GET ALL USERS
  * URL: http://localhost:3000/users
  * Method: GET
  */
-router.get('/', usersConroller.getAllUsers);
+router.get('/', usersController.getAllUsers);
 
 /**
  * GET USER
  * URL: http://localhost:3000/users/:id
  * Method: GET
  */
-router.get('/:id', usersConroller.getUserById);
+router.get('/:id', usersController.getUserById);
 
 /**
  * DELETE USER
  * URL: http://localhost:3000/users/:id
  * Method: DELETE
  */
-router.delete('/:id', usersConroller.deleteUser);
+router.delete('/:id', usersController.deleteUser);
 
 /**
  * UPDATE USER
  * URL: http://localhost:3000/users/:id
  * Method: PUT
  */
-router.put('/:id', usersConroller.updateUser);
+router.put('/:id', usersController.updateUser);
 
 module.exports = router;
